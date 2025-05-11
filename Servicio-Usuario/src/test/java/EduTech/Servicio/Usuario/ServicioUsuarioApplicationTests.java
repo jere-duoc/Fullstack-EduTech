b2eb02775f24e78783d21cb5e08fa9640ebf5293
jere <jer.silvaa@duocuc.cl>
2025-05-11 19:05:57 -0400

    Servicios Pagos y usuarios
    Framework de los Servicios Pagos y usuarios

---
+++ Servicio-Usuario/src/test/java/EduTech/Servicio/Usuario/ServicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package EduTech.Servicio.Usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
