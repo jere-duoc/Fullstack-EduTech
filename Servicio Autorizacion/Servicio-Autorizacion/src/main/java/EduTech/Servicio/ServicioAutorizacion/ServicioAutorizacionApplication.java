72e644dae1f3e3a0d5e9ed24cd76b7dbaa101dcf
Jiancardeus <jia.rivera@duocuc.cl>
2025-05-14 01:14:53 -0400

    Servicio-Autorizacion
    lo ma basico pa la wea de autorizacion, pero basico basico nos falta configurar las bd pa ir testeando owo

--- Servicio Autorizacion/Servicio-Autorizacion/src/main/java/EduTech/Servicio/ServicioAutorizacion/ServicioAutorizacionApplication.java
+++ Servicio Autorizacion/Servicio-Autorizacion/src/main/java/EduTech/Servicio/ServicioAutorizacion/ServicioAutorizacionApplication.java
@@ -1,13 +1,13 @@
-package EduTech.Servicio.Pagos;
+package EduTech.ServicioAutorizacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicioPagosApplication {
+public class ServicioAutorizacionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicioPagosApplication.class, args);
+		SpringApplication.run(ServicioAutorizacionApplication.class, args);
 	}
 
 }
---
+++ Servicio Autorizacion/Servicio-Autorizacion/src/main/java/EduTech/Servicio/ServicioAutorizacion/repository/AutorizacionRepository.java
@@ -0,0 +1,10 @@
+package EduTech.ServicioAutorizacion.repository;
+
+import EduTech.ServicioAutorizacion.entity.Autorizacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorizacionRepository extends JpaRepository<Autorizacion, Long> {
+    // Puedes agregar métodos personalizados para consultas específicas
+}
