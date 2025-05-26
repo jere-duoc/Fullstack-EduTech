975407439be94accc38c07b6c883ad4fa6b2bae4
isaacIRV <i.rivas@duocuc.cl>
2025-05-25 21:34:06 -0400

    a

--- Servicio-Usuario/src/main/java/EduTech/Servicio/Usuario/Controller/UsuarioController.java
+++ Servicio-Usuario/src/main/java/EduTech/Servicio/Usuario/Controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import EduTech.Servicio.Usuario.Model.Usuario;
 import EduTech.Servicio.Usuario.Service.UsuarioService;
 
 @RestController
